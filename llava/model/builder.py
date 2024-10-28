d184c0c2aa7a836466e16d611e9f35e9a0372676
yuelin bai <baiyl98@outlook.com>
2024-10-28 04:47:23 +0000

    update

--- llava/model/builder.py
+++ llava/model/builder.py
@@ -278,6 +278,7 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
     image_processor = None
 
     if "llava" in model_name.lower() or is_multimodal or "pangea" in model_name.lower() or "pulse" in model_name.lower():
+        print("### loading vision tower...")
         mm_use_im_start_end = getattr(model.config, "mm_use_im_start_end", False)
         mm_use_im_patch_token = getattr(model.config, "mm_use_im_patch_token", True)
         if mm_use_im_patch_token:
