519640a005a4f1d921764d1c98254f95650d1cd9
yuelin bai <baiyl98@outlook.com>
2024-10-28 13:10:02 +0800

    Update builder.py

--- llava/model/builder.py
+++ llava/model/builder.py
@@ -278,22 +278,22 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
     image_processor = None
 
     print("### model name",model_name.lower())
-    if "llava" in model_name.lower() or is_multimodal or "pangea" in model_name.lower() or "pulse" in model_name.lower():
-        print("### loading vision tower...")
-        mm_use_im_start_end = getattr(model.config, "mm_use_im_start_end", False)
-        mm_use_im_patch_token = getattr(model.config, "mm_use_im_patch_token", True)
-        if mm_use_im_patch_token:
-            tokenizer.add_tokens([DEFAULT_IMAGE_PATCH_TOKEN], special_tokens=True)
-        if mm_use_im_start_end:
-            tokenizer.add_tokens([DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN], special_tokens=True)
-        model.resize_token_embeddings(len(tokenizer))
+    # if "llava" in model_name.lower() or is_multimodal or "pangea" in model_name.lower() or "pulse" in model_name.lower():
+    print("### loading vision tower...")
+    mm_use_im_start_end = getattr(model.config, "mm_use_im_start_end", False)
+    mm_use_im_patch_token = getattr(model.config, "mm_use_im_patch_token", True)
+    if mm_use_im_patch_token:
+        tokenizer.add_tokens([DEFAULT_IMAGE_PATCH_TOKEN], special_tokens=True)
+    if mm_use_im_start_end:
+        tokenizer.add_tokens([DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN], special_tokens=True)
+    model.resize_token_embeddings(len(tokenizer))
 
-        vision_tower = model.get_vision_tower()
-        if not vision_tower.is_loaded:
-            vision_tower.load_model(device_map=device_map)
-        if device_map != "auto":
-            vision_tower.to(device="cuda", dtype=torch.float16)
-        image_processor = vision_tower.image_processor
+    vision_tower = model.get_vision_tower()
+    if not vision_tower.is_loaded:
+        vision_tower.load_model(device_map=device_map)
+    if device_map != "auto":
+        vision_tower.to(device="cuda", dtype=torch.float16)
+    image_processor = vision_tower.image_processor
 
     if hasattr(model.config, "max_sequence_length"):
         context_len = model.config.max_sequence_length
